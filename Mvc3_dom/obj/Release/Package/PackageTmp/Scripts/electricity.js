5c19404ec33fffac92658c5b1a16c777f14f4955
wdv388 <wdv388@yahoo.com>
2016-06-14 16:00:54 +0300

    Added me project

---
+++ Mvc3_dom/obj/Release/Package/PackageTmp/Scripts/electricity.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    $('#Difference').hover(function () {
+        var cur = parseInt($('#Current').val());
+        var previus = parseInt($('#Previous').val());
+        var difference = $('#Difference').val(cur - previus);
+        console.log(curtotal, previus, difference);
+    });
+
+    $('#TotaltoPay').hover(function () {
+        var difference = parseInt($('#Difference').val());
+        var price = parseFloat($('#Price').val());
+        var total = $('#TotaltoPay').val(difference * price)
+    });
+
+
+});
