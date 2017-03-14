40e5021ee26dec32b59754be138c7b575404e901
unknown <wdv388@yahoo.com>
2017-03-14 16:21:17 +0200

    Added new section to controler

---
+++ Mvc3_dom/obj/Release/Package/PackageTmp/Scripts/cold.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+
+    $('#CurrentTotal').hover(function () {
+       
+        var cur =parseInt( $('#Current').val());
+        var cur1 = parseInt( $('#Current1').val());
+        var total = $('input#CurrentTotal').val(cur + cur1);
+        console.log(total);
+    });
+    $('#Difference').hover(function () {
+        var curtotal = parseInt($('#CurrentTotal').val());
+        var previus = parseInt($('#Previous').val());
+        var difference = $('#Difference').val(curtotal - previus);
+        console.log(curtotal,previus,difference);
+    });
+
+    $('#TotaltoPay').hover(function () {
+        var difference = parseInt($('#Difference').val());
+        var price= parseFloat ($('#Price').val());
+        var total = $('#TotaltoPay').val(difference*price)
+    });
+
+});
